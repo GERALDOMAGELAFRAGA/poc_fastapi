f2910a49f6c33157b1bb326c2e6ef0bd11922cc5
GERALDOMAGELAFRAGA <geraldomagelafraga@outlook.com>
2024-01-27 17:46:46 -0300

    path parameters 1

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {'message':'Hello World'}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {'user_id':'the current user'}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {'user_id': user_id}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {'item_id':item_id}
